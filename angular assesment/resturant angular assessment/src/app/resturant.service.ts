59f5bd7fddcf3130c7403d2e288dcad7cfcef476
kavyaraavi <40602114+kavyaraavi@users.noreply.github.com>
2019-10-31 10:28:40 +0530

    Add files via upload

---
+++ angular assesment/resturant angular assessment/src/app/resturant.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Resturant } from './Resturant.model';
+@Injectable({
+  providedIn: 'root'
+})
+export class ResturantService {
+
+  constructor(private http: HttpClient) { }
+
+  saveResturant(resturant: Resturant) {
+    return this.http.post<any>('http://localhost:1068/resturant', resturant);
+  }
+
+  updateResturant(resturant: Resturant) {
+    return this.http.put<Resturant>('http://localhost:1068/resturant', resturant);
+  }
+
+  getAllResturants() {
+    return this.http.get<Resturant[]>('http://localhost:1068/resturant');
+  }
+
+
+  deleteResturant(resturantId: number) {
+    return this.http.delete(`http://localhost:1068/resturant/${resturantId}`);
+  }
+
+  getResturant(resturantId: number) {
+    return this.http.get<Resturant>(`http://localhost:1068/resturant/${resturantId}`);
+  }
+
+}
